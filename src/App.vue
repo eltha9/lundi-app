7079ff3ea0cc1c55753804d141374bbbb6e0fba7
eltha9 <theadosto@gmail.com>
2022-05-23 21:58:28 +0200

    adding icon font

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,26 @@
 <template>
-  <div id="app" class="text-semantic-positive-500 font-sato">
-    <span>test view</span>
-    <div>
-      <Avatar large />
-      <AvatarStack />
-      <Btn> tewt</Btn>
-    </div>
-  </div>
+	<div id="app" class="text-semantic-positive-500 font-sato">
+		<span>test view</span>
+		<div>
+			<Avatar large />
+			<AvatarStack />
+			<!-- <Btn> tewt</Btn> -->
+			<div>icon <i class="icon-call text-secondary-500"></i></div>
+		</div>
+	</div>
 </template>
 
 <style lang="scss"></style>
 
 <script>
-import Avatar from "@/components/lundi-uiKit/avatar/Avatar";
-import AvatarStack from "@/components/lundi-uiKit/avatar/AvatarStack";
-import Btn from "@/components/lundi-uiKit/Button";
-export default {
-  components: {
-    Avatar,
-    AvatarStack,
-    Btn,
-  },
-};
+	import Avatar from "@/components/lundi-uiKit/avatar/Avatar";
+	import AvatarStack from "@/components/lundi-uiKit/avatar/AvatarStack";
+	// import Btn from "@/components/lundi-uiKit/Button";
+	export default {
+		components: {
+			Avatar,
+			AvatarStack,
+			// Btn,
+		},
+	};
 </script>
