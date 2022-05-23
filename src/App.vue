ca7c0052192ca9ea2edfe2a9b28b21741dc1c4fe
eltha9 <theadosto@gmail.com>
2022-05-23 17:07:40 +0200

    wip button component, warn crash when setting color

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,25 @@
 <template>
-	<div id="app" class="text-semantic-positive-500 font-sato">
-		<span>test view</span>
-		<div>
-			<Avatar large />
-			<AvatarStack />
-		</div>
-	</div>
+  <div id="app" class="text-semantic-positive-500 font-sato">
+    <span>test view</span>
+    <div>
+      <Avatar large />
+      <AvatarStack />
+      <Btn> tewt</Btn>
+    </div>
+  </div>
 </template>
 
 <style lang="scss"></style>
 
 <script>
-	import Avatar from '@/components/lundi-uiKit/avatar/Avatar';
-	import AvatarStack from '@/components/lundi-uiKit/avatar/AvatarStack';
-	export default {
-		components: {
-			Avatar,
-			AvatarStack,
-		},
-	};
+import Avatar from "@/components/lundi-uiKit/avatar/Avatar";
+import AvatarStack from "@/components/lundi-uiKit/avatar/AvatarStack";
+import Btn from "@/components/lundi-uiKit/Button";
+export default {
+  components: {
+    Avatar,
+    AvatarStack,
+    Btn,
+  },
+};
 </script>
---
+++ src/components/lundi-uiKit/Button.vue
@@ -0,0 +1,66 @@
+<template>
+  <button class="btn" :style="variables">
+    <slot name="left"> </slot>
+    <slot> Btn text </slot>
+    <slot name="right"> </slot>
+  </button>
+</template>
+
+<style lang="scss" scoped>
+.btn {
+  &.icon {
+    border-radius: 100%;
+  }
+  box-sizing: border-box;
+  @apply var(--bg-500);
+  @apply text-greyscale-white;
+  //   events
+  &:hover {
+    @apply var(--bg-700);
+  }
+  &:focus {
+    border: 1px solid;
+    @apply var(--text-300);
+  }
+  &:active {
+  }
+  &:disabled {
+    @apply bg-greyscale-200 text-greyscale-400;
+    @apply var(--bg-700);
+  }
+}
+</style>
+
+<script>
+// import Icon from "./Icon";
+export default {
+  name: "Btn",
+  //   components:{Icon},
+  props: {
+    icon: {
+      type: Boolean,
+      require: false,
+      default: false,
+    },
+    primary: {
+      type: Boolean,
+      require: false,
+      default: true,
+    },
+    color: {
+      type: String,
+      require: false,
+      default: "primary",
+    },
+  },
+  computed: {
+    variables() {
+      return {
+        "--bg-500": `bg-${this.color}-500`,
+        "--text-300": `text-${this.color}-300`,
+        "--bg-700": `bg-${this.color}-700`,
+      };
+    },
+  },
+};
+</script>
