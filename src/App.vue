5d0ff3dd06e6dab36e7bedbf3d91780cdb230ac6
eltha9 <theadosto@gmail.com>
2022-05-23 12:10:23 +0200

    adding avatar, avatar stack, update eslint rules

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,22 @@
 <template>
-	<div id="app" class="text-semantic-positive-500 font-sato">landing view</div>
+	<div id="app" class="text-semantic-positive-500 font-sato">
+		<span>test view</span>
+		<div>
+			<Avatar large />
+			<AvatarStack />
+		</div>
+	</div>
 </template>
 
 <style lang="scss"></style>
+
+<script>
+	import Avatar from '@/components/lundi-uiKit/avatar/Avatar';
+	import AvatarStack from '@/components/lundi-uiKit/avatar/AvatarStack';
+	export default {
+		components: {
+			Avatar,
+			AvatarStack,
+		},
+	};
+</script>
