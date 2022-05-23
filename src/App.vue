d697f21cf371c2d81085b0d3a377118d7b26b20f
eltha9 <theadosto@gmail.com>
2022-05-23 22:27:10 +0200

    add grid layouts

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,34 @@
 <template>
-	<div id="app" class="text-semantic-positive-500 font-sato">
-		<span>test view</span>
-		<div>
-			<Avatar large />
-			<AvatarStack />
-			<!-- <Btn> tewt</Btn> -->
-			<div>icon <i class="icon-call text-secondary-500"></i></div>
+	<div id="app" class="font-sato main-view dashboard">
+		<div class="text-semantic-positive-500">
+			<span>test view</span>
+			<div>
+				<Avatar large />
+				<AvatarStack />
+				<!-- <Btn> tewt</Btn> -->
+				<div>icon <i class="icon-call text-secondary-500"></i></div>
+			</div>
 		</div>
 	</div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+	div.main-view {
+		height: 100%;
+		width: 100%;
+		padding: 0 80px;
+		display: grid;
+		grid-template-columns: repeat(12, 1fr);
+		gap: 16px;
+
+		&.dashboard {
+			padding: 0 32px;
+			&.collapsed {
+				padding: 0 40px;
+			}
+		}
+	}
+</style>
 
 <script>
 	import Avatar from "@/components/lundi-uiKit/avatar/Avatar";
