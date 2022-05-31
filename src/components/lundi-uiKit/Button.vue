759e6fd9e4ce50f8ac1b0bec4117068b0f0acf54
eltha9 <theadosto@gmail.com>
2022-05-31 22:22:28 +0200

    add ternary button

--- src/components/lundi-uiKit/Button.vue
+++ src/components/lundi-uiKit/Button.vue
@@ -62,13 +62,60 @@
 				border: 1px solid var(--border-disable);
 			}
 		}
+		&.ternary {
+			background-color: transparent;
+			color: var(--text);
+			padding: 0;
+			border: 1px solid transparent;
+			border-radius: 0;
+			padding-bottom: 8px;
+			&:hover {
+				color: var(--text-hover);
+				border-bottom: 1px solid var(--border-hover);
+			}
+			&:focus {
+				color: var(--text-focus);
+				border-bottom: 1px solid var(--border-focus);
+			}
+			&:disabled {
+				color: var(--text-disable);
+			}
+			&:disabled:hover {
+				color: var(--text-disable);
+				border: 1px solid transparent;
+			}
+
+			&.icon {
+				color: var(--text);
+				padding: 4px;
+				overflow: hidden;
+				border-radius: 50%;
+
+				&:hover {
+					background-color: var(--bg-hover);
+					color: var(--text-hover);
+				}
+				&:focus {
+					background-color: var(--bg-focus);
+					color: var(--text-focus);
+				}
+				&:disabled {
+					color: var(--text-disable);
+					background-color: var(--bg-disable);
+				}
+				&:disabled:hover {
+					color: var(--text-disable);
+					background-color: var(--bg-disable);
+				}
+			}
+		}
 	}
 </style>
 
 <script>
-	import tailwindConfig from '@/../tailwind.config';
+	import tailwindConfig from "@/../tailwind.config";
 	export default {
-		name: 'Btn',
+		name: "Btn",
 		props: {
 			icon: {
 				type: Boolean,
@@ -85,10 +132,15 @@
 				require: false,
 				default: false,
 			},
+			ternary: {
+				type: Boolean,
+				require: false,
+				default: false,
+			},
 			color: {
 				type: String,
 				require: false,
-				default: 'primary',
+				default: "primary",
 			},
 			disabled: {
 				type: Boolean,
@@ -98,41 +150,97 @@
 		},
 		computed: {
 			variables() {
-				const colorData = this.color.split('-');
+				const colorData = this.color.split("-");
 				let color = tailwindConfig.theme.colors;
 				for (const colorPart of colorData) {
 					color = color[colorPart];
 				}
 				console.log(color);
+				if (this.secondary) {
+					return {
+						// default
+						"--bg": tailwindConfig.theme.colors.greyscale.white,
+						"--border": color["500"],
+						"--text": color["700"],
+						//on hover
+						"--bg-hover": tailwindConfig.theme.colors.greyscale.white,
+						"--border-hover": color["700"],
+						"--text-hover": color["700"],
+						// on press
+						"--bg-pressed": tailwindConfig.theme.colors.greyscale.white,
+						"--border-pressed": color["500"],
+						"--text-pressed": color["700"],
+						// on focus
+						"--bg-focus": tailwindConfig.theme.colors.greyscale.white,
+						"--border-focus": color["500"],
+						"--text-focus": color["700"],
+						// disable
+						"--bg-disable": tailwindConfig.theme.colors.greyscale.white,
+						"--border-disable": tailwindConfig.theme.colors.greyscale["400"],
+						"--text-disable": tailwindConfig.theme.colors.greyscale["500"],
+					};
+				}
+				if (this.ternary) {
+					return {
+						// default
+						"--bg": tailwindConfig.theme.colors.greyscale.white,
+						"--border": tailwindConfig.theme.colors.greyscale.white,
+						"--text": this.icon ? tailwindConfig.theme.colors.greyscale.black : color["700"],
+						//on hover
+						"--bg-hover": this.icon ? color["100"] : tailwindConfig.theme.colors.greyscale.white,
+						"--border-hover": this.icon ? tailwindConfig.theme.colors.greyscale.white : color["500"],
+						"--text-hover": this.icon ? tailwindConfig.theme.colors.greyscale.black : color["500"],
+						// on press
+						"--bg-pressed": this.icon ? color["300"] : tailwindConfig.theme.colors.greyscale.white,
+						"--border-pressed": this.icon ? tailwindConfig.theme.colors.greyscale.white : color["700"],
+						"--text-pressed": this.icon ? tailwindConfig.theme.colors.greyscale.white : color["700"],
+						// on focus
+						"--bg-focus": this.icon ? color["100"] : tailwindConfig.theme.colors.greyscale.white,
+						"--border-focus": tailwindConfig.theme.colors.greyscale.white,
+						"--text-focus": this.icon ? tailwindConfig.theme.colors.greyscale.black : color["700"],
+						// disable
+						"--bg-disable": tailwindConfig.theme.colors.greyscale.white,
+						"--border-disable": tailwindConfig.theme.colors.greyscale.white,
+						"--text-disable": tailwindConfig.theme.colors.greyscale["500"],
+					};
+				}
 				return {
 					// default
-					'--bg': this.secondary ? tailwindConfig.theme.colors.greyscale.white : color[500],
-					'--border': this.secondary ? color[500] : color[500],
-					'--text': this.secondary ? color[700] : tailwindConfig.theme.colors.greyscale.white,
+					"--bg": color["500"],
+					"--border": color["500"],
+					"--text": tailwindConfig.theme.colors.greyscale.white,
 					//on hover
-					'--bg-hover': this.secondary ? tailwindConfig.theme.colors.greyscale.white : color[700],
-					'--border-hover': this.secondary ? color[700] : color[700],
-					'--text-hover': this.secondary ? color[700] : tailwindConfig.theme.colors.greyscale.white,
+					"--bg-hover": color["700"],
+					"--border-hover": color["700"],
+					"--text-hover": tailwindConfig.theme.colors.greyscale.white,
 					// on press
-					'--bg-pressed': this.secondary ? tailwindConfig.theme.colors.greyscale.white : color[500],
-					'--border-pressed': this.secondary ? color[500] : color[500],
-					'--text-pressed': this.secondary ? color[700] : tailwindConfig.theme.colors.greyscale.white,
+					"--bg-pressed": color["500"],
+					"--border-pressed": color["500"],
+					"--text-pressed": tailwindConfig.theme.colors.greyscale.white,
 					// on focus
-					'--bg-focus': this.secondary ? tailwindConfig.theme.colors.greyscale.white : color[500],
-					'--border-focus': this.secondary ? color[500] : color[300],
-					'--text-focus': this.secondary ? color[700] : tailwindConfig.theme.colors.greyscale.white,
+					"--bg-focus": color["500"],
+					"--border-focus": color["300"],
+					"--text-focus": tailwindConfig.theme.colors.greyscale.white,
 					// disable
-					'--bg-disable': this.secondary ? tailwindConfig.theme.colors.greyscale.white : tailwindConfig.theme.colors.greyscale['200'],
-					'--border-disable': this.secondary ? tailwindConfig.theme.colors.greyscale['400'] : tailwindConfig.theme.colors.greyscale['400'],
-					'--text-disable': this.secondary ? tailwindConfig.theme.colors.greyscale['500'] : tailwindConfig.theme.colors.greyscale['400'],
+					"--bg-disable": tailwindConfig.theme.colors.greyscale["200"],
+					"--border-disable": tailwindConfig.theme.colors.greyscale["400"],
+					"--text-disable": tailwindConfig.theme.colors.greyscale["400"],
 				};
 			},
 			buttonClasses() {
 				let classes = [];
-				if (this.icon) classes.push('icon');
-				if (this.secondary) classes.push('secondary');
+				if (this.icon) classes.push("icon");
+				if (this.secondary) {
+					classes.push("secondary");
+					return classes.join(" ");
+				}
+				if (this.ternary) {
+					classes.push("ternary");
+					return classes.join(" ");
+				}
+				if (this.primary) classes.push("primary");
 
-				return classes.join(' ');
+				return classes.join(" ");
 			},
 		},
 	};
--- src/views/other/Test.vue
+++ src/views/other/Test.vue
@@ -7,7 +7,15 @@
 				<!-- <AvatarStack :users="['', '', '', '', '', '']" /> -->
 				<Btn color="semantic-info" icon secondary> <i class="icon-call"></i></Btn>
 				<Btn color="semantic-warning" secondary class="mb-2"> test secondary</Btn>
-				<Btn color="ternary" primary> test primary</Btn>
+				<Btn color="ternary" primary class="mb-2"> test primary</Btn>
+				<Btn color="ternary" ternary class="mb-2">
+					<span>right ternary</span>
+					<template #right> <i class="icon-arrow-right"></i> </template>
+				</Btn>
+				<Btn color="ternary" ternary class="mb-2" disabled> ternary</Btn>
+
+				<Btn color="semantic-info" icon ternary class="mb-2" disabled> <i class="icon-call"></i></Btn>
+
 				<div>icon <i class="icon-call text-secondary-500"></i></div>
 			</div>
 			<router-link to="/dashboard">Go to dashboard</router-link>
@@ -17,11 +25,11 @@
 </template>
 
 <script>
-	import Avatar from '@/components/lundi-uiKit/avatar/Avatar';
-	import AvatarStack from '@/components/lundi-uiKit/avatar/AvatarStack';
-	import Btn from '@/components/lundi-uiKit/Button';
+	import Avatar from "@/components/lundi-uiKit/avatar/Avatar";
+	import AvatarStack from "@/components/lundi-uiKit/avatar/AvatarStack";
+	import Btn from "@/components/lundi-uiKit/Button";
 	export default {
-		name: 'Test',
+		name: "Test",
 		components: {
 			Avatar,
 			AvatarStack,
