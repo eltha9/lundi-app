e19d18343f81e375b4894f1e3b9798d925cd885b
eltha9 <theadosto@gmail.com>
2022-06-08 11:11:44 +0200

    wip input component

---
+++ src/components/lundi-uiKit/L-input.vue
@@ -0,0 +1,35 @@
+<template>
+	<div v-if="type === 'text'" class="l-input">
+		<input type="text" />
+	</div>
+	<div v-else-if="type === 'phone'" class="l-input">
+		<input type="text" />
+	</div>
+	<div v-else-if="type === 'password'" class="l-input">
+		<input type="text" />
+	</div>
+	<div v-else-if="type === 'select'" class="l-input">
+		<input type="text" />
+	</div>
+	<div v-else class="l-input">an error occuried</div>
+</template>
+<style lang="scss" scoped>
+	.l-input {
+	}
+</style>
+
+<script>
+	export default {
+		name: 'l-nnput',
+		props: {
+			type: {
+				require: true,
+				type: String,
+				default: 'text',
+			},
+		},
+		data() {
+			re;
+		},
+	};
+</script>
