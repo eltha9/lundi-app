64b232ebdf8474a5f63824d376baa5d3d10f8744
eltha9 <theadosto@gmail.com>
2022-06-16 15:50:33 +0200

    wip dashboard ui

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,39 @@
 <template>
-	<div id="app" class="text-greyscale-black bg-background-100">
-		<router-view></router-view>
-	</div>
+  <div id="app" class="text-greyscale-black bg-background-100">
+    <router-view></router-view>
+  </div>
 </template>
 
 <style lang="scss">
-	#app {
-		height: 100vh;
-		width: 100%;
-	}
-	div.main-view,
-	main.main-view {
-		height: 100%;
-		width: 100%;
-		padding: 0 80px;
-		display: grid;
-		grid-template-columns: repeat(12, 1fr);
-		gap: 16px;
-		overflow: auto;
-		overflow-x: hidden;
-		&.dashboard {
-			overflow: hidden;
-			padding: 0 32px;
-			padding-top: 48px;
-			&.collapsed {
-				padding: 0 40px;
-				padding-top: 48px;
-			}
-		}
-	}
+#app {
+  height: 100vh;
+  width: 100%;
+}
+div.main-view,
+main.main-view {
+  height: 100%;
+  width: 100%;
+  padding: 0 80px;
+  display: grid;
+  grid-template-columns: repeat(12, 1fr);
+  gap: 16px;
+  row-gap: 0;
+  overflow: auto;
+  overflow-x: hidden;
+  &.dashboard {
+    overflow: hidden;
+    padding: 0 32px;
+    padding-top: 48px;
+    &.collapsed {
+      padding: 0 40px;
+      padding-top: 48px;
+    }
+  }
+}
 </style>
 
 <script>
-	export default {
-		components: {},
-	};
+export default {
+  components: {},
+};
 </script>
