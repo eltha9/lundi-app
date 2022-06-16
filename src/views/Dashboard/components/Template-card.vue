d361583adeb698c7b26cd996b96a1405b60c1392
eltha9 <theadosto@gmail.com>
2022-06-16 17:01:20 +0200

    wip template card

--- src/views/Dashboard/components/Template-card.vue
+++ src/views/Dashboard/components/Template-card.vue
@@ -1,12 +1,33 @@
 <template>
-	<div class="template-card">stat card</div>
+  <div class="template-card">
+    <div class="top-card">
+      <div class="info">
+        <div class="status"></div>
+        <h4></h4>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
-	.template-card {
-	}
+.template-card {
+}
 </style>
 
 <script>
-	export default {};
+export default {
+	props:{
+		infos:{
+			require: true,
+			type: Object,
+			default: ()=>{
+				name:"",
+				status:""
+				lastUpdate:"",
+				tags:[],
+				users:[]
+			}
+		}
+	}
+};
 </script>
