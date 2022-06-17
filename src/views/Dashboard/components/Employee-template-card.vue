ea4f15e9131beb4f11469f7af45608a8834813a6
eltha9 <theadosto@gmail.com>
2022-06-17 16:15:51 +0200

    add progress bar component, add employee template card

--- src/views/Dashboard/components/Employee-template-card.vue
+++ src/views/Dashboard/components/Employee-template-card.vue
@@ -1,83 +1,100 @@
 <template>
-	<div class="employee-template-card text-greyscale-200 bg-greyscale-white p-5" :style="{ gridColumn: `${enhancedPosition.start}/${enhancedPosition.end}` }">
-		<div class="top-card flex justify-between mb-14">
-			<div class="info">
-				<h4 class="salva-h4 text-greyscale-800 mb-2 capitalize">{{ template.name }}</h4>
-				<div class="date text-greyscale-500 sato-l-m">
-					<i class="icon-update mr-1"></i>
-					<span>{{ lastUpdate }} jours</span>
-				</div>
-			</div>
-			<btn icon ternary class="option-btn" @click.native.stop=""><i class="icon-options-vertical"></i></btn>
-		</div>
-		<div class="bottom-card flex"></div>
-	</div>
+  <div
+    class="employee-template-card text-greyscale-200 bg-greyscale-white p-5"
+    :style="{ gridColumn: `${enhancedPosition.start}/${enhancedPosition.end}` }"
+  >
+    <div class="top-card flex justify-between mb-12">
+      <div class="info">
+        <h4 class="salva-h4 text-greyscale-800 mb-2 capitalize">
+          {{ template.name }}
+        </h4>
+        <div class="date text-greyscale-500 sato-l-m">
+          <i class="icon-update mr-1"></i>
+          <span>{{ lastUpdate }} jours</span>
+        </div>
+      </div>
+    </div>
+    <div class="bottom-card flex justify-between items-center">
+      <l-progress-bar :progress="template.progress" class="flex-1" />
+      <div
+        class="flex items-center font-bold text-greyscale-500 sato-l-s ml-24"
+      >
+        <i class="icon-work mr-1"></i>
+        <span class="capitalize">{{ template.teamName }}</span>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
-	.employee-template-card {
-		border: 1px solid;
-		border-radius: 8px;
-		grid-column: 1/5;
-		.top-card {
-			.info {
-				.team-name {
-					i {
-						// font-size: 16px;
-					}
-				}
-			}
-			.option-btn {
-				font-size: 20px;
-			}
-		}
-	}
+.employee-template-card {
+  border: 1px solid;
+  border-radius: 8px;
+  grid-column: 1/5;
+  .top-card {
+    .info {
+      .team-name {
+        i {
+          // font-size: 16px;
+        }
+      }
+    }
+    i.icon-work {
+      font-size: 16px;
+    }
+  }
+}
 </style>
 
 <script>
-	import Btn from "@/components/lundi-uiKit/Button.vue";
-	import AvatarStack from "@/components/lundi-uiKit/avatar/AvatarStack.vue";
-	export default {
-		components: {
-			Btn,
-			AvatarStack,
-		},
-		props: {
-			template: {
-				required: true,
-				type: Object,
-				default: () => ({
-					id: "",
-					name: "",
-					isFav: false,
-					lastUpdate: "",
-					users: [],
-				}),
-			},
-			position: {
-				required: false,
-				type: Number,
-				default: 1,
-			},
-		},
-		computed: {
-			lastUpdate() {
-				const todayStamp = parseInt(Date.now() / 1000);
-				const templateStamp = parseInt(new Date(this.team.lastUpdate).getTime() / 1000);
-				return Math.max(parseInt((todayStamp - templateStamp) / 60 / 60 / 24), 1) || "X";
-			},
-			enhancedPosition() {
-				switch (this.position % 3) {
-					case 1:
-						return { start: 1, end: 5 };
-					case 2:
-						return { start: 5, end: 9 };
-					case 0:
-						return { start: 9, end: 13 };
-					default:
-						return { start: 1, end: 5 };
-				}
-			},
-		},
-	};
+import AvatarStack from "@/components/lundi-uiKit/avatar/AvatarStack.vue";
+import LProgressBar from "@/components/lundi-uiKit/L-porgressBar.vue";
+export default {
+  components: {
+    AvatarStack,
+    LProgressBar,
+  },
+  props: {
+    template: {
+      required: true,
+      type: Object,
+      default: () => ({
+        id: "",
+        name: "",
+        lastUpdate: "",
+        progress: 0,
+        teamName: "",
+      }),
+    },
+    position: {
+      required: false,
+      type: Number,
+      default: 1,
+    },
+  },
+  computed: {
+    lastUpdate() {
+      const todayStamp = parseInt(Date.now() / 1000);
+      const templateStamp = parseInt(
+        new Date(this.template.lastUpdate).getTime() / 1000
+      );
+      return (
+        Math.max(parseInt((todayStamp - templateStamp) / 60 / 60 / 24), 1) ||
+        "X"
+      );
+    },
+    enhancedPosition() {
+      switch (this.position % 3) {
+        case 1:
+          return { start: 1, end: 5 };
+        case 2:
+          return { start: 5, end: 9 };
+        case 0:
+          return { start: 9, end: 13 };
+        default:
+          return { start: 1, end: 5 };
+      }
+    },
+  },
+};
 </script>
--- src/views/other/Test.vue
+++ src/views/other/Test.vue
@@ -1,255 +1,319 @@
 <template>
-	<div class="p-6">
-		<div class="text-semantic-positive-500 bg-background-100">
-			<span>test view</span>
-			<div>
-				<Avatar large />
-				<AvatarStack :users="['a', 'z', 'e', 'r', 't', 'y']" />
-				<Btn color="semantic-info" icon secondary> <i class="icon-call"></i></Btn>
-				<Btn color="semantic-warning" secondary class="mb-2"> test secondary</Btn>
-				<Btn color="ternary" primary class="mb-2"> test primary</Btn>
-				<Btn color="ternary" ternary class="mb-2">
-					<span>right ternary</span>
-					<template #right> <i class="icon-arrow-right"></i> </template>
-				</Btn>
-				<Btn color="ternary" ternary class="mb-2" disabled> ternary</Btn>
+  <div class="p-6">
+    <div class="text-semantic-positive-500 bg-background-100">
+      <span>test view</span>
+      <div>
+        <Avatar large />
+        <AvatarStack :users="['a', 'z', 'e', 'r', 't', 'y']" />
+        <Btn color="semantic-info" icon secondary>
+          <i class="icon-call"></i
+        ></Btn>
+        <Btn color="semantic-warning" secondary class="mb-2">
+          test secondary</Btn
+        >
+        <Btn color="ternary" primary class="mb-2"> test primary</Btn>
+        <Btn color="ternary" ternary class="mb-2">
+          <span>right ternary</span>
+          <template #right> <i class="icon-arrow-right"></i> </template>
+        </Btn>
+        <Btn color="ternary" ternary class="mb-2" disabled> ternary</Btn>
 
-				<Btn color="semantic-info" icon ternary class="mb-2" disabled> <i class="icon-call"></i></Btn>
+        <Btn color="semantic-info" icon ternary class="mb-2" disabled>
+          <i class="icon-call"></i
+        ></Btn>
 
-				<div>icon <i class="icon-call text-secondary-500"></i></div>
-			</div>
-			<router-link to="/dashboard">Go to dashboard</router-link>
-			<div class="sato-p-xs">test de text</div>
+        <div>icon <i class="icon-call text-secondary-500"></i></div>
+      </div>
+      <router-link to="/dashboard">Go to dashboard</router-link>
+      <div class="sato-p-xs">test de text</div>
 
-			<span>navbar test</span>
-			<nav-bar v-model="navBarTest" :items="['gauche', 'droite', 'en bas', 'en haut']" />
+      <span>navbar test</span>
+      <nav-bar
+        v-model="navBarTest"
+        :items="['gauche', 'droite', 'en bas', 'en haut']"
+      />
+      <l-progress-bar :progress="100" />
+      <!-- test input -->
+      <div class="mt-8">
+        <h3>input test</h3>
+        <l-select
+          v-model="selectModel"
+          name="test"
+          :options="['a', 'b', 'c']"
+        />
+        <l-input v-model="textModel" name="test" class="mt-4" />
+        <l-input
+          v-model="passwordModel"
+          name="Mot de passe"
+          type="password"
+          class="mt-4"
+        />
+        <l-input
+          v-model="phoneModel"
+          name="numéro de téléphone"
+          type="phone"
+          class="mt-4"
+        />
+        <l-file class="mt-4" placeholder="votre photo" v-model="fileModel" />
+        <l-checkbox class="mt-4" v-model="checkBoxModel" />
+      </div>
 
-			<!-- test input -->
-			<div class="mt-8">
-				<h3>input test</h3>
-				<l-select v-model="selectModel" name="test" :options="['a', 'b', 'c']" />
-				<l-input v-model="textModel" name="test" class="mt-4" />
-				<l-input v-model="passwordModel" name="Mot de passe" type="password" class="mt-4" />
-				<l-input v-model="phoneModel" name="numéro de téléphone" type="phone" class="mt-4" />
-				<l-file class="mt-4" placeholder="votre photo" v-model="fileModel" />
-				<l-checkbox class="mt-4" v-model="checkBoxModel" />
-			</div>
+      <h2 class="salva-h2 text-greyscale-black mt-8">test card</h2>
+      <div class="mt-2">
+        <h3>employee card</h3>
+        <div class="main-view">
+          <employee-card />
+        </div>
 
-			<h2 class="salva-h2 text-greyscale-black mt-8">test card</h2>
-			<div class="mt-2">
-				<h3>employee card</h3>
-				<div class="main-view">
-					<employee-card />
-				</div>
+        <h3>employee template card</h3>
+        <div class="main-view template-card-test">
+          <employee-template-card
+            :template="{
+              id: 'ahzgeja',
+              name: 'temlate name',
+              lastUpdate: '2022-03-02',
+              progress: 20,
+              teamName: 'team design',
+            }"
+            :position="1"
+          />
+          <employee-template-card
+            :template="{
+              id: 'ahzgeja',
+              name: 'temlate name',
+              lastUpdate: '2022-03-02',
+              progress: 80,
+              teamName: 'team design',
+            }"
+            :position="2"
+          />
+          <employee-template-card
+            :template="{
+              id: 'ahzgeja',
+              name: 'temlate name',
+              lastUpdate: '2022-03-02',
+              progress: 45,
+              teamName: 'team design',
+            }"
+            :position="3"
+          />
+          <employee-template-card
+            :template="{
+              id: 'ahzgeja',
+              name: 'temlate name',
+              lastUpdate: '2022-03-02',
+              progress: 90,
+              teamName: 'team design',
+            }"
+            :position="4"
+          />
+        </div>
 
-				<h3>employee template card</h3>
-				<div class="main-view">
-					<employee-template-card />
-				</div>
+        <h3>stat card</h3>
+        <div class="flex">
+          <stat-card
+            :stat="{
+              name: 'arrové',
+              mainValue: 21,
+              mainUnit: '%',
+              secondaryValue: 78,
+              secondaryUnit: 'nouveau',
+              link: 'dashboard-analytics',
+            }"
+          />
+          <stat-card
+            :stat="{
+              name: 'arrové',
+              mainValue: -21,
+              mainUnit: '',
+              secondaryValue: -5,
+              secondaryUnit: 'nouveau',
+              link: 'dashboard-analytics',
+            }"
+          />
+        </div>
 
-				<h3>stat card</h3>
-				<div class="flex">
-					<stat-card
-						:stat="{
-							name: 'arrové',
-							mainValue: 21,
-							mainUnit: '%',
-							secondaryValue: 78,
-							secondayUnit: 'nouveau',
-							link: 'dashboard-analytics',
-						}"
-					/>
-					<stat-card
-						:stat="{
-							name: 'arrové',
-							mainValue: -21,
-							mainUnit: '',
-							secondaryValue: -5,
-							secondayUnit: 'nouveau',
-							link: 'dashboard-analytics',
-						}"
-					/>
-				</div>
+        <h3>team card</h3>
+        <div class="main-view template-card-test">
+          <team-card
+            :team="{
+              id: 'geayrgor',
+              name: 'First Team',
+              isFav: true,
+              lastUpdate: '2022-05-23',
+              users: ['4', '5'],
+            }"
+            :position="1"
+          />
+          <team-card
+            :team="{
+              id: 'geayrgor',
+              name: 'First Team',
+              isFav: true,
+              lastUpdate: '2022-05-23',
+              users: ['4', '5'],
+            }"
+            :position="2"
+          />
+          <team-card
+            :team="{
+              id: 'geayrgor',
+              name: 'First Team',
+              isFav: false,
+              lastUpdate: '2022-05-23',
+              users: ['4', '5'],
+            }"
+            :position="3"
+          />
+          <team-card
+            :team="{
+              id: 'geayrgor',
+              name: 'First Team',
+              isFav: true,
+              lastUpdate: '2022-05-23',
+              users: ['4', '5'],
+            }"
+            :position="4"
+          />
+        </div>
 
-				<h3>team card</h3>
-				<div class="main-view template-card-test">
-					<team-card
-						:team="{
-							id: 'geayrgor',
-							name: 'First Team',
-							isFav: true,
-							lastUpdate: '2022-05-23',
-							users: ['4', '5'],
-						}"
-						:position="1"
-					/>
-					<team-card
-						:team="{
-							id: 'geayrgor',
-							name: 'First Team',
-							isFav: true,
-							lastUpdate: '2022-05-23',
-							users: ['4', '5'],
-						}"
-						:position="2"
-					/>
-					<team-card
-						:team="{
-							id: 'geayrgor',
-							name: 'First Team',
-							isFav: false,
-							lastUpdate: '2022-05-23',
-							users: ['4', '5'],
-						}"
-						:position="3"
-					/>
-					<team-card
-						:team="{
-							id: 'geayrgor',
-							name: 'First Team',
-							isFav: true,
-							lastUpdate: '2022-05-23',
-							users: ['4', '5'],
-						}"
-						:position="4"
-					/>
-				</div>
+        <h3>template card</h3>
+        <div class="main-view template-card-test">
+          <template-card
+            :infos="{
+              name: 'template test',
+              status: 'toAssign',
+              lastUpdate: '2022-06-01',
+              tags: ['Tout', 'Junior'],
+              users: ['a', 'b', 'c'],
+            }"
+            :position="1"
+          />
+          <template-card
+            :infos="{
+              name: 'template test',
+              status: 'toAssign',
+              lastUpdate: '2022-06-01',
+              tags: ['Tout', 'Junior'],
+              users: ['a', 'b', 'c'],
+            }"
+            :position="2"
+          />
+          <template-card
+            :infos="{
+              name: 'template test',
+              status: 'toAssign',
+              lastUpdate: '2022-06-01',
+              tags: ['Tout', 'Junior'],
+              users: ['a', 'b', 'c'],
+            }"
+            :position="3"
+          />
+          <template-card
+            :infos="{
+              name: 'template test',
+              status: 'toAssign',
+              lastUpdate: '2022-06-01',
+              tags: ['Tout', 'Junior'],
+              users: ['a', 'b', 'c'],
+            }"
+            :position="4"
+          />
+          <template-card
+            :infos="{
+              name: 'template test',
+              status: 'toAssign',
+              lastUpdate: '2022-06-01',
+              tags: ['Tout', 'Junior'],
+              users: ['a', 'b', 'c'],
+            }"
+            :position="5"
+          />
+        </div>
 
-				<h3>template card</h3>
-				<div class="main-view template-card-test">
-					<template-card
-						:infos="{
-							name: 'template test',
-							status: 'toAssign',
-							lastUpdate: '2022-06-01',
-							tags: ['Tout', 'Junior'],
-							users: ['a', 'b', 'c'],
-						}"
-						:position="1"
-					/>
-					<template-card
-						:infos="{
-							name: 'template test',
-							status: 'toAssign',
-							lastUpdate: '2022-06-01',
-							tags: ['Tout', 'Junior'],
-							users: ['a', 'b', 'c'],
-						}"
-						:position="2"
-					/>
-					<template-card
-						:infos="{
-							name: 'template test',
-							status: 'toAssign',
-							lastUpdate: '2022-06-01',
-							tags: ['Tout', 'Junior'],
-							users: ['a', 'b', 'c'],
-						}"
-						:position="3"
-					/>
-					<template-card
-						:infos="{
-							name: 'template test',
-							status: 'toAssign',
-							lastUpdate: '2022-06-01',
-							tags: ['Tout', 'Junior'],
-							users: ['a', 'b', 'c'],
-						}"
-						:position="4"
-					/>
-					<template-card
-						:infos="{
-							name: 'template test',
-							status: 'toAssign',
-							lastUpdate: '2022-06-01',
-							tags: ['Tout', 'Junior'],
-							users: ['a', 'b', 'c'],
-						}"
-						:position="5"
-					/>
-				</div>
+        <h3>contexted stat card</h3>
+        <div class="main-view">
+          <contexted-stat-card />
+        </div>
 
-				<h3>contexted stat card</h3>
-				<div class="main-view">
-					<contexted-stat-card />
-				</div>
+        <h3>graph card</h3>
+        <div class="main-view">
+          <graph-card />
+        </div>
 
-				<h3>graph card</h3>
-				<div class="main-view">
-					<graph-card />
-				</div>
-
-				<h3>simple stat card</h3>
-				<div class="main-view">
-					<simple-stat-card />
-				</div>
-			</div>
-		</div>
-	</div>
+        <h3>simple stat card</h3>
+        <div class="main-view">
+          <simple-stat-card />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <style lang="scss" scoped>
-	.template-card-test {
-		row-gap: 16px;
-	}
+.template-card-test {
+  row-gap: 16px;
+}
 </style>
 
 <script>
-	import Avatar from "@/components/lundi-uiKit/avatar/Avatar";
-	import AvatarStack from "@/components/lundi-uiKit/avatar/AvatarStack";
-	import Btn from "@/components/lundi-uiKit/Button";
-	import NavBar from "@/components/lundi-uiKit/NavBar";
-	import LSelect from "@/components/lundi-uiKit/inputs/L-select.vue";
-	import LInput from "@/components/lundi-uiKit/inputs/L-input.vue";
-	import LFile from "@/components/lundi-uiKit/inputs/L-file.vue";
-	import LCheckbox from "@/components/lundi-uiKit/inputs/L-checkbox.vue";
-	//card test
-	import EmployeeCard from "@/views/Dashboard/components/Employee-card.vue";
-	import EmployeeTemplateCard from "@/views/Dashboard/components/Employee-template-card.vue";
-	import StatCard from "@/views/Dashboard/components/Stat-card.vue";
-	import TeamCard from "@/views/Dashboard/components/Team-card.vue";
-	import TemplateCard from "@/views/Dashboard/components/Template-card.vue";
+import Avatar from "@/components/lundi-uiKit/avatar/Avatar";
+import AvatarStack from "@/components/lundi-uiKit/avatar/AvatarStack";
+import Btn from "@/components/lundi-uiKit/Button";
+import NavBar from "@/components/lundi-uiKit/NavBar";
+import LSelect from "@/components/lundi-uiKit/inputs/L-select.vue";
+import LInput from "@/components/lundi-uiKit/inputs/L-input.vue";
+import LFile from "@/components/lundi-uiKit/inputs/L-file.vue";
+import LCheckbox from "@/components/lundi-uiKit/inputs/L-checkbox.vue";
+import LProgressBar from "@/components/lundi-uiKit/L-porgressBar.vue";
+//card test
+import EmployeeCard from "@/views/Dashboard/components/Employee-card.vue";
+import EmployeeTemplateCard from "@/views/Dashboard/components/Employee-template-card.vue";
+import StatCard from "@/views/Dashboard/components/Stat-card.vue";
+import TeamCard from "@/views/Dashboard/components/Team-card.vue";
+import TemplateCard from "@/views/Dashboard/components/Template-card.vue";
 
-	import ContextedStatCard from "@/views/Dashboard/components/Stats/Contexted-stat-card.vue";
-	import GraphCard from "@/views/Dashboard/components/Stats/Graph-card.vue";
-	import SimpleStatCard from "@/views/Dashboard/components/Stats/Simple-stat-card.vue";
+import ContextedStatCard from "@/views/Dashboard/components/Stats/Contexted-stat-card.vue";
+import GraphCard from "@/views/Dashboard/components/Stats/Graph-card.vue";
+import SimpleStatCard from "@/views/Dashboard/components/Stats/Simple-stat-card.vue";
 
-	export default {
-		name: "Test",
-		components: {
-			Avatar,
-			AvatarStack,
-			Btn,
-			NavBar,
-			LSelect,
-			LInput,
-			LFile,
-			LCheckbox,
-			// cardTest
-			EmployeeCard,
-			EmployeeTemplateCard,
-			StatCard,
-			TeamCard,
-			TemplateCard,
+export default {
+  name: "Test",
+  components: {
+    Avatar,
+    AvatarStack,
+    Btn,
+    NavBar,
+    LSelect,
+    LInput,
+    LFile,
+    LCheckbox,
+    LProgressBar,
+    // cardTest
+    EmployeeCard,
+    EmployeeTemplateCard,
+    StatCard,
+    TeamCard,
+    TemplateCard,
 
-			ContextedStatCard,
-			GraphCard,
-			SimpleStatCard,
-		},
-		data() {
-			return {
-				navBarTest: "",
-				selectModel: "",
-				textModel: "",
-				passwordModel: "",
-				phoneModel: "",
-				fileModel: "",
-				checkBoxModel: true,
-			};
-		},
-		watch: {
-			textModel() {
-				console.log(this.selectModel);
-			},
-		},
-	};
+    ContextedStatCard,
+    GraphCard,
+    SimpleStatCard,
+  },
+  data() {
+    return {
+      navBarTest: "",
+      selectModel: "",
+      textModel: "",
+      passwordModel: "",
+      phoneModel: "",
+      fileModel: "",
+      checkBoxModel: true,
+    };
+  },
+  watch: {
+    textModel() {
+      console.log(this.selectModel);
+    },
+  },
+};
 </script>
