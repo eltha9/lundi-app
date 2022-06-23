f5afacbf608db58062f0920e12668674bac7e8dd
eltha9 <theadosto@gmail.com>
2022-06-23 19:12:45 +0200

    init analytics routes and view component

---
+++ src/views/Dashboard/subViews/Analytics/Member.vue
@@ -0,0 +1,20 @@
+<template>
+  <main
+    class="main-view dashboard"
+    :class="isSidebarCollapsed ? 'collapsed ' : ''"
+  >
+    member analytics page
+  </main>
+</template>
+
+<style lang="scss" scoped></style>
+
+<script>
+import { mapState } from "vuex";
+export default {
+  name: "DashboardAnalytics",
+  computed: {
+    ...mapState(["isSidebarCollapsed"]),
+  },
+};
+</script>
