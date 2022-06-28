d4f5ba18051a17f6b5926ba6ae6d3768394beef0
eltha9 <theadosto@gmail.com>
2022-06-28 08:21:16 +0200

    add graph no data

--- src/views/Dashboard/components/Stats/Camamber-card.vue
+++ src/views/Dashboard/components/Stats/Camamber-card.vue
@@ -1,5 +1,25 @@
 <template>
-	<div class="camamber-card text-greyscale-300 bg-greyscale-white p-6">camamber card</div>
+	<div class="camamber-card bg-greyscale-white text-greyscale-300 p-6 flex flex-col">
+		<div class="top mb-10">
+			<h4 class="salva-h4 text-greyscale-800">Progression global</h4>
+		</div>
+		<div class="graph flex-1 flex justify-center items-center">
+			<span class="sato-l-m font-bold text-greyscale-black">Pas encore de données pour le moment.</span>
+		</div>
+		<div class="bottom flex justify-between mt-10">
+			<div class="flex flex-col">
+				<span class="mb-2 saro-l-m font-bold text-greyscale-800">Durée de l'onboarding</span>
+				<div class="sato-l-m text-greyscale-600">
+					<span class="mr-1 salva-h3 text-greyscale-700">{{ lastUpdate(me.createdDate) }}</span
+					>Jours
+				</div>
+			</div>
+			<div class="flex flex-col">
+				<span class="mb-2 saro-l-m font-bold text-greyscale-800">Date d'arrivée</span>
+				<span class="sato-l-m text-greyscale-600">{{ formatDate(me.createdDate) }}</span>
+			</div>
+		</div>
+	</div>
 </template>
 
 <style lang="scss" scoped>
@@ -10,5 +30,21 @@
 </style>
 
 <script>
-	export default {};
+	import { mapState } from "vuex";
+	export default {
+		computed: {
+			...mapState(["me"]),
+		},
+		methods: {
+			lastUpdate(date) {
+				const todayStamp = parseInt(Date.now() / 1000);
+				const templateStamp = parseInt(new Date(date).getTime() / 1000);
+				return Math.max(parseInt((todayStamp - templateStamp) / 60 / 60 / 24), 1) || "X";
+			},
+			formatDate(date) {
+				const split = date.split("-");
+				return `${split[2]}/${split[1]}/${split[0]}`;
+			},
+		},
+	};
 </script>
--- src/views/Dashboard/components/Stats/Graph-card.vue
+++ src/views/Dashboard/components/Stats/Graph-card.vue
@@ -1,5 +1,14 @@
 <template>
-	<div class="graph-card text-greyscale-300 bg-greyscale-white p-6">stat card</div>
+	<div class="graph-card text-greyscale-300 bg-greyscale-white p-6 flex flex-col">
+		<h4 class="salva-h4 text-greyscale-800 mb-1">Arrivées en cours et à venir</h4>
+		<div class="flex justify-between text-greyscale-600 sato-l-m">
+			<span>sur la période</span>
+			<span>Aucun onboarding en cours</span>
+		</div>
+		<div class="flex-1 flex justify-center items-center">
+			<span class="sato-l-m font-bold text-greyscale-black">Pas encore de données pour le moment.</span>
+		</div>
+	</div>
 </template>
 
 <style lang="scss" scoped>
