efe434af7538f8f16c771ec1bfe95d50effd6a92
eltha9 <theadosto@gmail.com>
2022-06-29 10:28:32 +0200

    wip

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,35 +1,35 @@
 export const ROLES = [
-  {
-    value: "admin",
-    name: "Administrateur",
-  },
-  {
-    value: "editor",
-    name: "Editeur",
-  },
-  {
-    value: "onboardee",
-    name: "Onboardé",
-  },
+	{
+		value: 'admin',
+		name: 'Administrateur',
+	},
+	{
+		value: 'editor',
+		name: 'Editeur',
+	},
+	{
+		value: 'onboardee',
+		name: 'Onboardé',
+	},
 ];
-export const SENIORITY = ["senior", "stagiaire/apparanti", "junior"];
+export const SENIORITY = ['senior', 'stagiaire/apprentie', 'junior'];
 export const TEMPLATE_STATUS = {
-  toAssign: {
-    name: "Prêt  à être assigner",
-    textColor: "text-semantic-positive-500",
-    pseudoBgColor: "bg-semantic-positive-500",
-    bgColor: "bg-semantic-positive-100",
-  },
-  inUpdate: {
-    name: "En cours de modification",
-    textColor: "text-semantic-warning-500",
-    pseudoBgColor: "bg-semantic-warning-500",
-    bgColor: "bg-semantic-warning-100",
-  },
-  toFinish: {
-    name: "A finir",
-    textColor: "text-semantic-negative-500",
-    pseudoBgColor: "bg-semantic-negative-500",
-    bgColor: "bg-semantic-negative-100",
-  },
+	toAssign: {
+		name: 'Prêt  à être assigner',
+		textColor: 'text-semantic-positive-500',
+		pseudoBgColor: 'bg-semantic-positive-500',
+		bgColor: 'bg-semantic-positive-100',
+	},
+	inUpdate: {
+		name: 'En cours de modification',
+		textColor: 'text-semantic-warning-500',
+		pseudoBgColor: 'bg-semantic-warning-500',
+		bgColor: 'bg-semantic-warning-100',
+	},
+	toFinish: {
+		name: 'A finir',
+		textColor: 'text-semantic-negative-500',
+		pseudoBgColor: 'bg-semantic-negative-500',
+		bgColor: 'bg-semantic-negative-100',
+	},
 };
--- src/lib/utilis.js
+++ src/lib/utilis.js
@@ -13,266 +13,245 @@ export const getUserInformation = (userId) => {
 		profilePic: '/assets/test/avatar.jpeg',
 		email: 'elmo@sesame.stree',
 		role: 'Lead dev',
-        userPerm:'admin',
-        lastActivity:"2022-06-20",
-        createdDate:"2022-03-02",
-        progress:50,
+		userPerm: 'onboardee',
+		lastActivity: '2022-06-20',
+		createdDate: '2022-03-02',
+		progress: 50,
 		id: userId,
-        templates:[
-            {
-                id: "temp1",
-                name:"Arriver",
-                teamName:"Team rh",
-                progress: 45,
-                docs:[
-                    "doc id"
-                ]
-            },
-            {
-                id: "temp3",
-                name:"Bonne conduite en entreprise",
-                teamName:"Team rh",
-                progress: 45,
-                docs:[
-                    "doc id"
-                ]
-            },
-            {
-                id: "temp2",
-                name:"Utilisation des Systèmes d'information",
-                teamName:"Team SI",
-                progress: 95,
-                docs:[
-                    "doc id"
-                ]
-            },
-            
-        ],
-        documents:[
-            {
-                name:"Contrat-de-travail.pdf",
-                uploadName:"contrat de travail",
-                uploadDate:"2022-06-22",
-                teamName:"Team rh",
-                templateName:"Arriver",
-                date:"2022-04-22",
-                status:"toLate",
-                id:"azeaze",
-            },
-            {
-                name:"chartre-informatique.pdf",
-                uploadName:"Chartre informatique",
-                uploadDate:"2022-06-22",
-                teamName:"Team rh",
-                templateName:"Arriver",
-                date:"2022-04-22",
-                status:"good",
-                id:"azeaze",
-            },
-        ]
+		templates: [
+			{
+				id: 'temp1',
+				name: 'Arriver',
+				teamName: 'Team rh',
+				lastUpdate: '2022-06-20',
+				progress: 45,
+				docs: ['doc id'],
+			},
+			{
+				id: 'temp3',
+				name: 'Bonne conduite en entreprise',
+				teamName: 'Team rh',
+				lastUpdate: '2022-06-20',
+				progress: 45,
+				docs: ['doc id'],
+			},
+			{
+				id: 'temp2',
+				name: "Utilisation des Systèmes d'information",
+				teamName: 'Team SI',
+				lastUpdate: '2022-06-20',
+				progress: 95,
+				docs: ['doc id'],
+			},
+		],
+		documents: [
+			{
+				name: 'Contrat-de-travail.pdf',
+				uploadName: 'contrat de travail',
+				uploadDate: '2022-06-22',
+				teamName: 'Team rh',
+				templateName: 'Arriver',
+				date: '2022-04-22',
+				status: 'toLate',
+				id: 'azeaze',
+			},
+			{
+				name: 'chartre-informatique.pdf',
+				uploadName: 'Chartre informatique',
+				uploadDate: '2022-06-22',
+				teamName: 'Team rh',
+				templateName: 'Arriver',
+				date: '2022-04-22',
+				status: 'good',
+				id: 'azeaze',
+			},
+		],
 	};
 	const data = httpRequest({url: `/user?id=${userId}`});
 	return data;
 };
-export const login =(psw,email)=>{
-    return {
-        jwt: "agzryozetrgouaeroayertg",
-        me:{
-            firstName: 'Jean Michel',
+export const login = (psw, email) => {
+	return {
+		jwt: 'agzryozetrgouaeroayertg',
+		me: {
+			firstName: 'Jean Michel',
+			lastName: 'Genestier',
+			profilePic: '/assets/test/jmg.jpg',
+			email: 'jm@le-raincy.fr',
+			role: 'Lead dev',
+			userPerm: 'onboardee',
+			lastActivity: '2022-06-20',
+			createdDate: '2022-03-02',
+			progress: 50,
+			id: 'az1',
+			templates: [
+				{
+					id: 'temp1',
+					name: 'Arriver',
+					teamName: 'Team rh',
+					progress: 45,
+					docs: ['doc id'],
+				},
+				{
+					id: 'temp3',
+					name: 'Bonne conduite en entreprise',
+					teamName: 'Team rh',
+					lastUpdate: '2022-06-20',
+					progress: 45,
+					docs: ['doc id'],
+				},
+				{
+					id: 'temp2',
+					name: '',
+					teamName: 'team plop',
+					lastUpdate: '2022-06-20',
+					progress: 95,
+					docs: ['doc id'],
+				},
+			],
+			documents: [
+				{
+					name: 'Contrat-de-travail.pdf',
+					uploadName: 'contrat de travail',
+					uploadDate: '2022-06-22',
+					teamName: 'Team rh',
+					templateName: 'Arriver',
+					date: '2022-04-22',
+					status: 'toLate',
+					id: 'azeaze',
+				},
+				{
+					name: 'chartre-informatique.pdf',
+					uploadName: 'Chartre informatique',
+					uploadDate: '2022-06-22',
+					teamName: 'Team rh',
+					templateName: 'Arriver',
+					date: '2022-04-22',
+					status: 'good',
+					id: 'azeaze',
+				},
+			],
+			settings: {
+				favs: [],
+			},
+		},
+	};
+	const data = httpRequest({url: `/login`});
+	return data;
+};
+export const getMe = (jwt) => {
+	return {
+		firstName: 'Jean Michel',
 		lastName: 'Genestier',
 		profilePic: '/assets/test/jmg.jpg',
 		email: 'jm@le-raincy.fr',
 		role: 'Lead dev',
-        userPerm:'admin',
-        lastActivity:"2022-06-20",
-        createdDate:"2022-03-02",
-        progress:50,
-		id: "az1",
-        templates:[
-            {
-                id: "temp1",
-                name:"Arriver",
-                teamName:"Team rh",
-                progress: 45,
-                docs:[
-                    "doc id"
-                ]
-            },
-            {
-                id: "temp3",
-                name:"Bonne conduite en entreprise",
-                teamName:"Team rh",
-                progress: 45,
-                docs:[
-                    "doc id"
-                ]
-            },
-            {
-                id: "temp2",
-                name:"",
-                teamName:"team plop",
-                progress: 95,
-                docs:[
-                    "doc id"
-                ]
-            },
-            
-        ],
-        documents:[
-            {
-                name:"Contrat-de-travail.pdf",
-                uploadName:"contrat de travail",
-                uploadDate:"2022-06-22",
-                teamName:"Team rh",
-                templateName:"Arriver",
-                date:"2022-04-22",
-                status:"toLate",
-                id:"azeaze",
-            },
-            {
-                name:"chartre-informatique.pdf",
-                uploadName:"Chartre informatique",
-                uploadDate:"2022-06-22",
-                teamName:"Team rh",
-                templateName:"Arriver",
-                date:"2022-04-22",
-                status:"good",
-                id:"azeaze",
-            },
-        ],
-            settings: {
-                favs: [
-                  
-                ],
-              },
-        },
-    };
-    const data = httpRequest({url: `/login`});
+		userPerm: 'onboardee',
+		lastActivity: '2022-06-20',
+		createdDate: '2022-03-02',
+		progress: 50,
+		id: 'az1',
+		templates: [
+			{
+				id: 'temp1',
+				name: 'Arriver',
+				teamName: 'Team rh',
+				progress: 45,
+				docs: ['doc id'],
+			},
+			{
+				id: 'temp3',
+				name: 'Bonne conduite en entreprise',
+				teamName: 'Team rh',
+				progress: 45,
+				docs: ['doc id'],
+			},
+			{
+				id: 'temp2',
+				name: '',
+				teamName: 'team plop',
+				progress: 95,
+				docs: ['doc id'],
+			},
+		],
+		documents: [
+			{
+				name: 'Contrat-de-travail.pdf',
+				uploadName: 'contrat de travail',
+				uploadDate: '2022-06-22',
+				teamName: 'Team rh',
+				templateName: 'Arriver',
+				date: '2022-04-22',
+				status: 'toLate',
+				id: 'azeaze',
+			},
+			{
+				name: 'chartre-informatique.pdf',
+				uploadName: 'Chartre informatique',
+				uploadDate: '2022-06-22',
+				teamName: 'Team rh',
+				templateName: 'Arriver',
+				date: '2022-04-22',
+				status: 'good',
+				id: 'azeaze',
+			},
+		],
+		settings: {
+			favs: [],
+		},
+	};
+	const data = httpRequest({url: `/me`});
 	return data;
-}
-export const getMe = (jwt)=>{
-    return {
-        firstName: 'Jean Michel',
-		lastName: 'Genestier',
-		profilePic: '/assets/test/jmg.jpg',
-		email: 'jm@le-raincy.fr',
-		role: 'Lead dev',
-        userPerm:'admin',
-        lastActivity:"2022-06-20",
-        createdDate:"2022-03-02",
-        progress:50,
-		id: "az1",
-        templates:[
-            {
-                id: "temp1",
-                name:"Arriver",
-                teamName:"Team rh",
-                progress: 45,
-                docs:[
-                    "doc id"
-                ]
-            },
-            {
-                id: "temp3",
-                name:"Bonne conduite en entreprise",
-                teamName:"Team rh",
-                progress: 45,
-                docs:[
-                    "doc id"
-                ]
-            },
-            {
-                id: "temp2",
-                name:"",
-                teamName:"team plop",
-                progress: 95,
-                docs:[
-                    "doc id"
-                ]
-            },
-            
-        ],
-        documents:[
-            {
-                name:"Contrat-de-travail.pdf",
-                uploadName:"contrat de travail",
-                uploadDate:"2022-06-22",
-                teamName:"Team rh",
-                templateName:"Arriver",
-                date:"2022-04-22",
-                status:"toLate",
-                id:"azeaze",
-            },
-            {
-                name:"chartre-informatique.pdf",
-                uploadName:"Chartre informatique",
-                uploadDate:"2022-06-22",
-                teamName:"Team rh",
-                templateName:"Arriver",
-                date:"2022-04-22",
-                status:"good",
-                id:"azeaze",
-            },
-        ],
-            settings: {
-                favs: [
-                  
-                ],
-              },
-    };
-    const data = httpRequest({url: `/me`});
+};
+export const getCompagnieInformation = (jwt) => {
+	return {
+		compagnie: {
+			name: 'Acme.co',
+			logo: '/assets/test/logo.svg',
+			teams: [
+				{
+					id: 'hazkeh',
+					name: 'Team rh',
+					users: ['id1sd', 'idsdqs1', 'idx1'],
+					lastUpdate: '2022-06-18',
+					isFav: false,
+				},
+				{
+					id: 'haazeazeazeh',
+					name: 'Team SI',
+					users: ['id1sd', 'idsdqs1', 'idx1'],
+					lastUpdate: '2022-06-18',
+					isFav: false,
+				},
+			],
+		},
+	};
+	const data = httpRequest({url: ``});
 	return data;
-}
-export const getCompagnieInformation= (jwt)=>{
-    return {
-        compagnie: {
-            name: "Robert Space Industry",
-            logo: "/assets/test/rsi.png",
-            teams: [
-              {
-                id: "hazkeh",
-                name: "Team rh",
-                users: ["id1sd", "idsdqs1", "idx1"],
-                lastUpdate: "2022-06-18",
-                isFav: false,
-              },
-              {
-                id: "haazeazeazeh",
-                name: "Team SI",
-                users: ["id1sd", "idsdqs1", "idx1"],
-                lastUpdate: "2022-06-18",
-                isFav: false,
-              }
-            ],
-          },
-    }
-    const data = httpRequest({url: ``});
+};
+export const getMyTeam = (jwt) => {
+	return {
+		name: 'Team rh',
+		id: 'auzgeiugaze',
+		members: [
+			{
+				firstName: 'Elmo',
+				lastName: '',
+				profilePic: '/assets/test/avatar.jpeg',
+				email: 'elmo@sesame.stree',
+				role: 'Lead dev',
+				id: 'cookied',
+			},
+			{
+				firstName: 'Mr',
+				lastName: 'Cookie',
+				profilePic: '',
+				email: 'cookie@cookie.cookie',
+				role: 'Senior dev',
+				id: 'yuerouryt',
+			},
+		],
+	};
+	const data = httpRequest({url: ``});
 	return data;
-}
-export const getMyTeam = (jwt)=>{
-    return {
-        name:"Team rh",
-        id:"auzgeiugaze",
-        members:[
-            {
-                firstName: 'Elmo',
-                lastName: '',
-                profilePic: '/assets/test/avatar.jpeg',
-                email: 'elmo@sesame.stree',
-                role: 'Lead dev',
-                id: "cookied",
-                
-            },
-            {
-                firstName: 'Mr',
-                lastName: 'Cookie',
-                profilePic: '',
-                email: 'cookie@cookie.cookie',
-                role: 'Senior dev',
-                id:"yuerouryt"
-            },
-        ]
-    }
-    const data = httpRequest({url: ``});
-	return data;
-}
+};
