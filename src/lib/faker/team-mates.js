ac3fc4763c18b7b8a69da284c35c7873236752b6
eltha9 <theadosto@gmail.com>
2022-06-30 11:36:10 +0200

    plop

--- src/lib/faker/team-mates.js
+++ src/lib/faker/team-mates.js
@@ -1,22 +1,22 @@
 export const MY_TEAM = {
-    name: 'Ressource Humaine',
-		id: 'auzgeiugaze',
-		members: [
-			{
-				firstName: 'Elmo',
-				lastName: '',
-				profilePic: '/assets/test/avatar.jpeg',
-				email: 'elmo@sesame.stree',
-				role: 'Lead dev',
-				id: 'cookied',
-			},
-			{
-				firstName: 'Mr',
-				lastName: 'Cookie',
-				profilePic: '',
-				email: 'cookie@cookie.cookie',
-				role: 'Senior dev',
-				id: 'yuerouryt',
-			},
-		],
-}
+	name: 'Ressource Humaine',
+	id: 'auzgeiugaze',
+	members: [
+		{
+			firstName: 'Noé',
+			lastName: 'C',
+			profilePic: '/assets/test/avatar.jpeg',
+			email: 'noe.c@gmail.stree',
+			role: 'Senior leead Manager',
+			id: 'mate-1',
+		},
+		{
+			firstName: 'Clément',
+			lastName: 'P',
+			profilePic: '',
+			email: 'clement.p@gmail.com',
+			role: 'Senior leead Manager',
+			id: 'mate-2',
+		},
+	],
+};
