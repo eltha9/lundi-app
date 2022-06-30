4be6d773c63729acaa438060e503ee7b8e00e2cf
eltha9 <theadosto@gmail.com>
2022-06-30 16:31:03 +0200

    add more information do faker users

--- src/views/Dashboard/components/Stats/Graph-card.vue
+++ src/views/Dashboard/components/Stats/Graph-card.vue
@@ -1,23 +1,27 @@
 <template>
-	<div class="graph-card text-greyscale-300 bg-greyscale-white p-6 flex flex-col">
-		<h4 class="salva-h4 text-greyscale-800 mb-1">Arrivées en cours et à venir</h4>
-		<div class="flex justify-between text-greyscale-600 sato-l-m">
-			<span>sur la période</span>
-			<span>Aucun onboarding en cours</span>
-		</div>
-		<div class="flex-1 flex justify-center items-center">
-			<span class="sato-l-m font-bold text-greyscale-black">Pas encore de données pour le moment.</span>
-		</div>
-	</div>
+  <div
+    class="graph-card text-greyscale-300 bg-greyscale-white p-6 flex flex-col"
+  >
+    <h4 class="salva-h4 text-greyscale-800 mb-1">
+      Arrivées en cours et à venir
+    </h4>
+    <div class="flex justify-between text-greyscale-600 sato-l-m">
+      <span>sur la période</span>
+      <span>Aucun onboarding en cours</span>
+    </div>
+    <div class="flex-1 flex justify-center items-center">
+      <img src="@/assets/Graph.png" class="h-100" />
+    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
-	.graph-card {
-		border: 1px solid;
-		border-radius: 12px;
-	}
+.graph-card {
+  border: 1px solid;
+  border-radius: 12px;
+}
 </style>
 
 <script>
-	export default {};
+export default {};
 </script>
