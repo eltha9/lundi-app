dcbdf1f86961ff01e87c566c035ae2111e57f259
eltha9 <theadosto@gmail.com>
2022-11-19 13:39:34 +0100

    working on vue3-typescript-pinia

--- src/lib/http.js
+++
@@ -1,9 +0,0 @@
-export const httpRequest = async ({url = '', method = 'GET', body = {}}) => {
-	const options = {
-		url,
-		method,
-		body,
-	};
-	const data = await fetch(options);
-	return data.json();
-};
